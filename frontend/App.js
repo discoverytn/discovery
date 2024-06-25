a1fd7564decdd6ce8573745b8a8c9396da1e0179
Selimbensaid <selimbensaid11@gmail.com>
2024-06-25 16:40:12 +0100

    authcontext,post functions for explorer and businessOwner in backend and addpost screen for explorer in front

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,11 @@
+
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { enableScreens } from 'react-native-screens';
-
+import AuthProvider from './context/AuthContext'; 
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import SignupScreen from './screens/SignupScreen';
@@ -13,6 +14,7 @@ import Intro2 from './screens/Intro2';
 import Intro3 from './screens/Intro3';
 import CategoriesScreen from './screens/CategoriesScreen';
 import DiscoverScreen from './screens/DiscoverScreen';
+import ExplorerAddPostScreen from "./screens/ExplorerAddPostScreen";
 
 enableScreens();
 
@@ -22,12 +24,13 @@ function MainNavigator() {
   return (
     <Tab.Navigator
       screenOptions={{
-        tabBarVisible: false, // this is deprecated but keeping it for older versions when testing
+        tabBarVisible: false,
         swipeEnabled: true,
         headerShown: false,
-        tabBarStyle: { display: 'none' }, // This also hides the tab bar
+        tabBarStyle: { display: 'none' },
       }}
     >
+      <Tab.Screen name="ExplorerAddPostScreen" component={ExplorerAddPostScreen} options={{ tabBarLabel: () => null }} />
       <Tab.Screen name="Home" component={HomeScreen} options={{ tabBarLabel: () => null }} />
       <Tab.Screen name="Intro1" component={Intro1} options={{ tabBarLabel: () => null }} />
       <Tab.Screen name="Intro2" component={Intro2} options={{ tabBarLabel: () => null }} />
@@ -36,7 +39,6 @@ function MainNavigator() {
       <Tab.Screen name="Signup" component={SignupScreen} options={{ tabBarLabel: () => null }} />
       <Tab.Screen name="Categories" component={CategoriesScreen} options={{ tabBarLabel: () => null }} />
       <Tab.Screen name="Discover" component={DiscoverScreen} options={{ tabBarLabel: () => null }} />
-
     </Tab.Navigator>
   );
 }
@@ -44,7 +46,9 @@ function MainNavigator() {
 export default function App() {
   return (
     <NavigationContainer>
-      <MainNavigator />
+      <AuthProvider>
+        <MainNavigator />
+      </AuthProvider>
       <StatusBar style="auto" />
     </NavigationContainer>
   );
