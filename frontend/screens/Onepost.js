172aa6d94be1ebd5059a5ef0fabe0ef30345a039
AzizChinguiti <chinguitiaziz@gmail.com>
2024-07-02 17:28:21 +0100

    one post css and favorites page css done

--- frontend/screens/Onepost.js
+++ frontend/screens/Onepost.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { View, Text, Image, TouchableOpacity, StyleSheet, ScrollView, Alert } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faHeart, faPaperPlane, faCloudSunRain, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faPaperPlane, faCloudSunRain, faStar, faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
 import { useNavigation } from '@react-navigation/native'; 
 import { useAuth } from '../context/AuthContext';
@@ -11,6 +11,7 @@ const OnepostScreen = ({ route }) => {
   const [selectedImage, setSelectedImage] = useState(null);
   const [showFullDescription, setShowFullDescription] = useState(false);
   const [isFavorited, setIsFavorited] = useState(false); 
+  const [showMoreReviews, setShowMoreReviews] = useState(false);
   const scrollViewRef = useRef(null);
   const { explorer } = useAuth(); 
   const navigation = useNavigation(); 
@@ -27,8 +28,7 @@ const OnepostScreen = ({ route }) => {
   const checkIfPostFavorited = async (postId) => {
     try {
       const idexplorer = explorer.idexplorer; 
-      const response = await axios.get(`http://192.168.100.3:3000/explorer/${idexplorer}/favourites/${postId}/isFavorited`);
-
+      const response = await axios.get(`http://192.168.1.19:3000/explorer/${idexplorer}/favourites/${postId}/check`);
       setIsFavorited(response.data.favorited); 
     } catch (error) {
       console.error('Error checking if post is favorited:', error);
@@ -36,27 +36,21 @@ const OnepostScreen = ({ route }) => {
   };
 
   if (!postData) {
-    return <Text>Loading...</Text>;
+    return  <View style={styles.loadingContainer}>
+    <Text style={styles.loadingText}>Onepost</Text>
+    <Text style={styles.noPostsText}>No post selected yet !</Text>
+  </View>
   }
 
   const { postId, postDetails } = postData;
-  const {
-    name,
-    location,
-    image,
-    image2,
-    image3,
-    image4,
-    description,
-  } = postDetails;
+  const { name, location, image, image2, image3, image4, description } = postDetails;
 
   const shortDescription = description.slice(0, 100) + '...';
 
   const addToFavorites = async () => {
     try {
       const idexplorer = explorer.idexplorer; 
-      
-      const response = await axios.post(`http://192.168.100.3:3000/explorer/${idexplorer}/favourites/${postId}/addOrRemove`, {
+      const response = await axios.post(`http://192.168.1.19:3000/explorer/${idexplorer}/favourites/${postId}/addOrRemove`, {
         idposts: postId,
       });
   
@@ -82,7 +76,6 @@ const OnepostScreen = ({ route }) => {
   };
 
   const handleSendPost = () => {
-    
     Alert.alert('Post sent', 'Your post has been sent successfully!');
   };
 
@@ -93,68 +86,102 @@ const OnepostScreen = ({ route }) => {
 
   return (
     <ScrollView ref={scrollViewRef} style={styles.container}>
-      <Image source={{ uri: selectedImage }} style={styles.selectedImage} />
-      <ScrollView horizontal style={styles.imageCarousel}>
-        <TouchableOpacity onPress={() => handleImagePress(image.uri)}>
-          <Image source={{ uri: image.uri }} style={styles.carouselImage} />
-        </TouchableOpacity>
-        {image2 && (
-          <TouchableOpacity onPress={() => handleImagePress(image2.uri)}>
-            <Image source={{ uri: image2.uri }} style={styles.carouselImage} />
-          </TouchableOpacity>
-        )}
-        {image3 && (
-          <TouchableOpacity onPress={() => handleImagePress(image3.uri)}>
-            <Image source={{ uri: image3.uri }} style={styles.carouselImage} />
-          </TouchableOpacity>
-        )}
-        {image4 && (
-          <TouchableOpacity onPress={() => handleImagePress(image4.uri)}>
-            <Image source={{ uri: image4.uri }} style={styles.carouselImage} />
-          </TouchableOpacity>
-        )}
-      </ScrollView>
-
-      <View style={styles.contentContainer}>
-        <View style={styles.iconRow}>
-          <TouchableOpacity onPress={addToFavorites}>
+      <View style={styles.imageContainer}>
+        <Image source={{ uri: selectedImage }} style={styles.mainImage} />
+        <View style={styles.iconsContainer}>
+          <TouchableOpacity onPress={addToFavorites} style={styles.iconContainer}>
             <FontAwesomeIcon
               icon={faHeart}
-              size={25}
-              color={isFavorited ? 'red' : 'black'} 
-              style={styles.icon}
+              style={[styles.icon, isFavorited ? styles.favoriteIconActive : styles.favoriteIconInactive]}
+              size={26}
             />
           </TouchableOpacity>
-          <FontAwesomeIcon
-            icon={faPaperPlane}
-            size={25}
-            style={styles.icon}
-            onPress={handleSendPost}
-          />
-          <FontAwesomeIcon
-            icon={faCloudSunRain}
-            size={25}
-            style={styles.icon}
-          />
-          <FontAwesomeIcon
-            icon={faStar}
-            size={25}
-            style={styles.icon}
-          />
+          <FontAwesomeIcon icon={faPaperPlane} style={styles.icon1} size={26} onPress={handleSendPost} />
         </View>
-        <Text style={styles.name}>{name}</Text>
-        <Text style={styles.location}>{location}</Text>
-        <Text style={styles.description}>
-          {showFullDescription ? description : shortDescription}
-          {description.length > 100 && (
-            <Text
-              style={styles.readMore}
-              onPress={() => setShowFullDescription(!showFullDescription)}
-            >
-              {showFullDescription ? ' Show less' : ' Read more'}
-            </Text>
+        <View style={styles.thumbnailContainer}>
+          {image2 && (
+            <TouchableOpacity onPress={() => handleImagePress(image2.uri)}>
+              <Image source={{ uri: image2.uri }} style={styles.thumbnail} />
+            </TouchableOpacity>
           )}
-        </Text>
+          {image3 && (
+            <TouchableOpacity onPress={() => handleImagePress(image3.uri)}>
+              <Image source={{ uri: image3.uri }} style={styles.thumbnail} />
+            </TouchableOpacity>
+          )}
+          {image4 && (
+            <TouchableOpacity onPress={() => handleImagePress(image4.uri)}>
+              <Image source={{ uri: image4.uri }} style={styles.thumbnail} />
+            </TouchableOpacity>
+          )}
+          {selectedImage !== image.uri && (
+            <TouchableOpacity onPress={() => handleImagePress(image.uri)}>
+              <Image source={{ uri: image.uri }} style={styles.thumbnail} />
+            </TouchableOpacity>
+          )}
+        </View>
+      </View>
+      <View style={styles.detailsContainer}>
+        <Text style={styles.title}>{name}</Text>
+        <View style={styles.infoRow}>
+          <View style={styles.infoItem}>
+            <Image source={require('../assets/location.jpg')} style={styles.infoIcon} />
+            <Text style={styles.infoText}>{location}</Text>
+          </View>
+          <View style={styles.infoItem}>
+            <FontAwesomeIcon icon={faCloudSunRain} style={styles.weatherIcon} size={32} />
+            <Text style={styles.infoText}>25°C</Text>
+          </View>
+          <View style={styles.infoItem}>
+            <View style={styles.eventIconContainer}>
+              <Image source={require('../assets/calender.jpg')} style={styles.eventIcon} />
+            </View>
+          </View>
+        </View>
+        <Text style={styles.subtitle}>About Destination</Text>
+        <Text style={styles.description}>{showFullDescription ? description : shortDescription}</Text>
+        <TouchableOpacity onPress={() => setShowFullDescription(!showFullDescription)}>
+          <Text style={styles.readMore}>{showFullDescription ? 'Read Less' : 'Read More'}</Text>
+        </TouchableOpacity>
+        <View style={styles.ratingRow}>
+          <FontAwesomeIcon icon={faStar} style={styles.starIcon} />
+          <Text style={styles.ratingText}>Rating</Text>
+          <Text style={styles.ratingValue}>4.5 out of 5</Text>
+        </View>
+        
+        <View style={styles.userRow}>
+          <Image source={require('../assets/user.jpg')} style={styles.profileImage} />
+          <View style={styles.userInfo}>
+            <Text style={styles.userName}>John Doe</Text>
+            <Text style={styles.userHandle}>@johndoe</Text>
+            <Text style={styles.comment}>Great place to visit! Highly recommend.</Text>
+          </View>
+          <FontAwesomeIcon icon={faTrash} style={styles.userIcon} />
+          <FontAwesomeIcon icon={faEdit} style={styles.userIcon} />
+        </View>
+        <TouchableOpacity style={styles.seeMoreButton} onPress={() => setShowMoreReviews(!showMoreReviews)}>
+          <Text style={styles.seeMoreText}>{showMoreReviews ? 'Hide reviews' : 'See more reviews'}</Text>
+        </TouchableOpacity>
+        {showMoreReviews && (
+          <View style={styles.reviewsContainer}>
+            <View style={styles.userRow}>
+              <Image source={require('../assets/user.jpg')} style={styles.profileImage} />
+              <View style={styles.userInfo}>
+                <Text style={styles.userName}>Jane Smith</Text>
+                <Text style={styles.userHandle}>@janesmith</Text>
+                <Text style={styles.comment}>Amazing experience, will come back!</Text>
+              </View>
+            </View>
+            <View style={styles.userRow}>
+              <Image source={require('../assets/user.jpg')} style={styles.profileImage} />
+              <View style={styles.userInfo}>
+                <Text style={styles.userName}>Alex Johnson</Text>
+                <Text style={styles.userHandle}>@alexjohnson</Text>
+                <Text style={styles.comment}>Beautiful scenery and great weather.</Text>
+              </View>
+            </View>
+          </View>
+        )}
       </View>
     </ScrollView>
   );
@@ -165,48 +192,181 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  selectedImage: {
+  imageContainer: {
+    position: 'relative',
+  },
+  mainImage: {
     width: '100%',
-    height: 400,
-    marginBottom: 10,
+    height: 350,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    marginBottom: 7,
   },
-  imageCarousel: {
-    flexDirection: 'row',
-    marginBottom: 10,
-  },
-  carouselImage: {
-    width: 100,
-    height: 100,
-    marginRight: 5,
-  },
-  contentContainer: {
-    padding: 20,
-  },
-  iconRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginBottom: 10,
+  iconsContainer: {
+    position: 'absolute',
+    top: 20,
+    right: 20,
+    flexDirection: 'column',
   },
   icon: {
-    marginRight: 10,
+    color: '#DB81B6',
+    marginBottom: 12,
+    marginTop: 11,
   },
-  name: {
-    fontSize: 24,
-    fontWeight: 'bold',
+  icon1: {
+    color: '#2ac00a',
+    marginBottom: 12,
+  },
+  thumbnailContainer: {
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+    flexDirection: 'column',
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    padding: 5,
+    borderRadius: 20,
+  },
+  thumbnail: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
     marginBottom: 5,
   },
-  location: {
+  detailsContainer: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  infoRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  },
+  infoItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  infoIcon: {
+    width: 20,
+    height: 20,
+    marginRight: 5,
+  },
+  infoText: {
+    fontSize: 16,
+    color: 'grey',
+    marginRight: 5,
+    fontStyle: 'italic',
+  },
+  weatherIcon: {
+    color: 'blue',
+    marginRight: 8,
+  },
+  eventIconContainer: {
+    padding: 5,
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderRadius: 10,
+  },
+  eventIcon: {
+    width: 35,
+    height: 35,
+  },
+  subtitle: {
     fontSize: 18,
-    color: '#888',
+    fontWeight: 'bold',
+    marginTop: 20,
     marginBottom: 10,
   },
   description: {
-    fontSize: 16,
-    color: '#666',
+    fontSize: 14,
+    color: 'grey',
   },
   readMore: {
-    color: '#888',
+    color: 'orange',
+    marginTop: 5,
   },
+  ratingRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  starIcon: {
+    color: 'yellow',
+    marginRight: 5,
+  },
+  ratingText: {
+    fontSize: 16,
+    marginRight: 5,
+  },
+  ratingValue: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  favoriteIconInactive: {
+    color: '#DB81B6',
+  },
+  favoriteIconActive: {
+    color: '#f00',
+  },
+  userRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  profileImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginRight: 10,
+  },
+  userInfo: {
+    flex: 1,
+  },
+  userName: {
+    fontWeight: 'bold',
+  },
+  userHandle: {
+    color: 'violet',
+  },
+  comment: {
+    color: 'grey',
+  },
+  userIcon: {
+    color: 'grey',
+    marginLeft: 10,
+  },
+  seeMoreButton: {
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderRadius: 5,
+    padding: 10,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  seeMoreText: {
+    color: 'grey',
+  },
+  reviewsContainer: {
+    marginTop: 20,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  loadingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  noPostsText: {
+    fontSize: 18,
+    color: 'grey',
+  }
 });
 
 export default OnepostScreen;
