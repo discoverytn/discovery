9183bf3eb7aa1ad4824560cce56c52963383af3a
Yusuf <youssefblur13@gmail.com>
2024-07-10 15:39:27 +0200

    successfully gettin ids from auth to eventList

--- frontend/App.js
+++ frontend/App.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React from 'react' ; 
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
